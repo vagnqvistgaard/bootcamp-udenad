fdd94f2f10646432eecab50ce9972e47c9e2f5c4
vagnqvistgaard <U773554@rts-365.dk>
2021-08-17 14:49:18 +0200

    add eks_01b - changes

--- eks_01b_DOMogEvents/js/index.js
+++ eks_01b_DOMogEvents/js/index.js
@@ -1,6 +1,30 @@
 // "DOMContentLoaded" event'en sørger for, at Javascriptet først kører når hele HTML dokumentet er indlæst
 document.addEventListener("DOMContentLoaded", () => {
 
-	// skriv din kode her...
+	let medianNum = document.querySelector('#text-count');
+
+	let numCount = 5;
+	let minCount = 0;
+	let maxCount = 10;
+
+	function updatenumCount() {
+		medianNum.innerHTML = parseInt(numCount);
+	}
+
+	updatenumCount();
+
+	document.querySelector('#btn-count-decrease').addEventListener('click', () => {
+		if (numCount > minCount) {
+			numCount--;
+			updatenumCount();
+		}
+	});
+
+	document.querySelector('#btn-count-increase').addEventListener('click', () => {
+		if (numCount < maxCount) {
+			numCount++;
+			updatenumCount();
+		}
+	});
 
 }); // Afslutter: DOMContentLoaded
