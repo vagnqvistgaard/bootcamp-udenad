55bfb071d5643d5b62b91b71a4c9bafdee214848
vagnqvistgaard <U773554@rts-365.dk>
2021-08-17 13:11:43 +0200

    Add eks_01a solution2

--- eks_01a_DOMogEvents/js/index.js
+++ eks_01a_DOMogEvents/js/index.js
@@ -2,6 +2,35 @@
 // "DOMContentLoaded" event'en sørger for, at Javascriptet først kører når hele HTML dokumentet er indlæst
 document.addEventListener("DOMContentLoaded", () => {
 
-	// skriv din kode her...
+	let medianNum = document.querySelector('#text-count');
+	let numCount = 5;
+
+	function updatenumCount() {
+		medianNum.innerHTML = parseInt(numCount);
+	}
+
+	updatenumCount();
+
+	document.querySelector('#btn-count-decrease').addEventListener('click', () => {
+		if (numCount > 0) {
+			numCount--;
+		}
+	});
+
+	document.querySelector('#btn-count-increase').addEventListener('click', () => {
+		if (numCount < 10) {
+			numCount++;
+		}
+	});
 
 }); // Afslutter: DOMContentLoaded
+
+
+/*
+Opret en funktion som indsætter tallet 5 i p-tagget med id 'text-count'
+klik plus = +1;
+klik minus = -1;
+ingen værdier >10 eller > 0;
+
+
+*/
