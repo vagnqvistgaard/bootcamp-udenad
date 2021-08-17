a8be13b88173d6a726b1bf71b9eba5b149f6a81c
vagnqvistgaard <U773554@rts-365.dk>
2021-08-17 18:06:35 +0200

    Add to eks_01c_DOMogEvents

--- eks_01c_DOMogEvents/js/index.js
+++ eks_01c_DOMogEvents/js/index.js
@@ -1,6 +1,38 @@
 // "DOMContentLoaded" event'en sørger for, at Javascriptet først kører når hele HTML dokumentet er indlæst
 document.addEventListener("DOMContentLoaded", () => {
 
-	// skriv din kode her...
+	let medianNum = document.querySelector('#text-count');
 
-}); // Afslutter: DOMContentLoaded
+	let numCount = 5;
+	let minCount = 0;
+	let maxCount = 10;
+
+	function updatenumCount() {
+		medianNum.innerHTML = parseInt(numCount);
+	}
+
+	function checkMinMax(t) {
+		if (t.innerHTML == '-') {
+			if (numCount != minCount) {
+				numCount--;
+			}
+		}
+		if (t.innerHTML == '+') {
+			if (numCount != maxCount) {
+				numCount++;
+			}
+		}
+		updatenumCount();
+	}
+
+	updatenumCount();
+
+	document.querySelector('#btn-count-decrease').addEventListener('click', (e) => {
+		checkMinMax(e.target);
+	});
+
+	document.querySelector('#btn-count-increase').addEventListener('click', (e) => {
+		checkMinMax(e.target);
+	});
+
+});// Afslutter: DOMContentLoaded
