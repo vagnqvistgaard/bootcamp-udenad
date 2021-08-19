68e81f88a77e487d4693e4a0461e326c2a2d564f
vagnqvistgaard <U773554@rts-365.dk>
2021-08-19 10:41:30 +0200

    Add solution eks_2a-funktioner

--- eks_02a_funktioner/js/index.js
+++ eks_02a_funktioner/js/index.js
@@ -3,4 +3,16 @@ document.addEventListener("DOMContentLoaded", () => {
 
 	// skriv din kode her...
 
+
+	function bigGreeting() {
+		console.log('Hello there: Lunch is imminent!');
+	}
+
+	bigGreeting();
+
 }); // Afslutter: DOMContentLoaded
+
+//opret func kaldt 'hils', skriv 'hej med dig' i console
+// kald funktiomen.
+// gør det noget, om der kaldes før eller efter oprettelse? Nej...
+// Der er tale om hoisting (og full load før aktivering);
