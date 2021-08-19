87507433885445e5340c7d45dbd5a85d3e7822f6
vagnqvistgaard <U773554@rts-365.dk>
2021-08-19 11:26:34 +0200

    Add changes eks_02b_funktioner

--- eks_02b_funktioner/js/index.js
+++ eks_02b_funktioner/js/index.js
@@ -2,5 +2,19 @@
 document.addEventListener("DOMContentLoaded", () => {
 
 	// skriv din kode her...
+	let name = 'Somebody';
+	let arr = ['ronda', 'harris'];
+
+	function bigGreeting() {
+		const streng = 'Hello there: Lunch is imminent,';
+		console.log(`${streng} ${arr[0]}`); //template literals
+		console.log(streng + ',' + arr[0]);
+	}
+
+	bigGreeting();
 
 }); // Afslutter: DOMContentLoaded
+
+
+//tilføj parameter 'navn', tilføj til log: 'hej med dig,' + navn
+// lav mellemrum efter komma....
