757381bc1399ce0c6548fcabcd3bfae4dffc25d0
vagnqvistgaard <U773554@rts-365.dk>
2021-08-19 14:13:29 +0200

    Add changes eks_03a

--- eks_03a_arrays/js/index.js
+++ eks_03a_arrays/js/index.js
@@ -3,4 +3,21 @@ document.addEventListener("DOMContentLoaded", () => {
 
 	// skriv din kode her...
 
+	let names = ['eeny', 'meeny', 'miney', 'moe'];
+
+	for (i = 0; i < names.length; i++) {
+		var el = document.createElement("LI");
+		el.textContent = names[i];
+		document.querySelector('#liste').appendChild(el);
+	}
+
+
+
+
+
+
 }); // Afslutter: DOMContentLoaded
+
+
+/* var der indeholder array m. tre navne
+loop for at udskrive  i ul-tag; husk li-tags om navnene */
